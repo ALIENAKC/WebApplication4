15caf9f626af35f90d3594a618744c156eec199c
Alen <akchurin_alen@mail.ru>
2022-05-26 14:22:43 +0300

    Добавьте файлы проекта.

---
+++ WebApplication4/UnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Uow;
+using MailChimp.Net.Models;
+using Microsoft.VisualStudio.Services.Account;
+using Octopus.Client.Repositories.Async;
+using System;
+
+public interface IRepository<TEntity>
+    where TEntity : IEntity
+{
+    Task<TEntity> GetById(Guid Id);
+    Task<IReadOnlyList<TEntity>> GetAll();
+    Task Add(TEntity entity);
+    Task Update(TEntity entity);
+}
